2696cf80a560be6e216ef4168ac41c8b0b79befb
matsutani <yushiro.ma2ta2.21@gmail.com>
2019-07-29 11:24:28 +0900

    delete unused method

--- lib/devise_sms_confirmable/models/sms_authenticatable.rb
+++ lib/devise_sms_confirmable/models/sms_authenticatable.rb
@@ -5,17 +5,6 @@ module Devise
     module SMSAuthenticatable
       extend ActiveSupport::Concern
 
-      BLACKLIST_FOR_SERIALIZATION = [:sms_confirmed_at, :sms_confirmation_sent_at, :unconfirmed_phone_number]
-
-      # Redefine inspect using serializable_hash, to ensure we don't accidentally
-      # leak passwords into exceptions.
-      def inspect
-        inspection = serializable_hash.collect do |k,v|
-          "#{k}: #{respond_to?(:attribute_for_inspect) ? attribute_for_inspect(k) : v.inspect}"
-        end
-        "#<#{self.class} #{inspection.join(", ")}>"
-      end
-
       protected
 
       def devise_texter
